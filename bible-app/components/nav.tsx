9d35b300167632c75edc1559567161e25de8baa1
Jerry <jamesharold1738@gmail.com>
2023-06-23 01:43:55 -0400

    added more css

--- bible-app/components/nav.tsx
+++ bible-app/components/nav.tsx
@@ -3,9 +3,8 @@ export default function nav(){
     return(
         <div>
               <nav>
-                <a href ='#About'> About</a>
-                <a href = '#Intro5'>User Story</a>
                 <h1  id = 'Heading'>Let God Speak to You </h1>
+                <h1 id = 'twoHeading'>#1 App for Worship and Prayer</h1>
               </nav>
         </div>
     )
